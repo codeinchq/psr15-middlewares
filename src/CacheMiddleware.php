1eeb477c4369d51ede7d932ce854318d297689e4
Joan Fabrégat <joan@codeinc.fr>
2018-03-04 08:46:24 +0100

    adding CacheMiddleware

---
+++ src/CacheMiddleware.php
@@ -0,0 +1,120 @@
+<?php
+//
+// +---------------------------------------------------------------------+
+// | CODE INC. SOURCE CODE                                               |
+// +---------------------------------------------------------------------+
+// | Copyright (c) 2017 - Code Inc. SAS - All Rights Reserved.           |
+// | Visit https://www.codeinc.fr for more information about licensing.  |
+// +---------------------------------------------------------------------+
+// | NOTICE:  All information contained herein is, and remains the       |
+// | property of Code Inc. SAS. The intellectual and technical concepts  |
+// | contained herein are proprietary to Code Inc. SAS are protected by  |
+// | trade secret or copyright law. Dissemination of this information or |
+// | reproduction of this material  is strictly forbidden unless prior   |
+// | written permission is obtained from Code Inc. SAS.                  |
+// +---------------------------------------------------------------------+
+//
+// Author:   Joan Fabrégat <joan@codeinc.fr>
+// Date:     04/03/2018
+// Time:     08:38
+// Project:  lib-psr15middlewares
+//
+declare(strict_types = 1);
+namespace CodeInc\Psr15Middlewares;
+use GuzzleHttp\Psr7\Response;
+use Micheh\Cache\CacheUtil;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+
+/**
+ * Class CacheMiddleware
+ *
+ * @uses CacheUtil
+ * @package CodeInc\Psr15Middlewares
+ * @author Joan Fabrégat <joan@codeinc.fr>
+ */
+class CacheMiddleware extends AbstractRecursiveMiddleware {
+	/**
+	 * @var \DateTime|null
+	 */
+	private $lastModified;
+
+	/**
+	 * @var string|null
+	 */
+	private $etag;
+
+	/**
+	 * CacheMiddleware constructor.
+	 *
+	 * @param null|MiddlewareInterface $nextMiddleware
+	 */
+	public function __construct(?MiddlewareInterface $nextMiddleware = null)
+	{
+		parent::__construct($nextMiddleware);
+	}
+
+	/**
+	 * @param \DateTime|null $lastModified
+	 */
+	public function setLastModified(?\DateTime $lastModified):void
+	{
+		$this->lastModified = $lastModified;
+	}
+
+	/**
+	 * @return \DateTime|null
+	 */
+	public function getLastModified():?\DateTime
+	{
+		return $this->lastModified;
+	}
+
+	/**
+	 * @param null|string $etag
+	 */
+	public function setEtag(?string $etag):void
+	{
+		$this->etag = $etag;
+	}
+
+	/**
+	 * @return null|string
+	 */
+	public function getEtag():?string
+	{
+		return $this->etag;
+	}
+
+	/**
+	 * @param ServerRequestInterface $request
+	 * @param RequestHandlerInterface $handler
+	 * @return ResponseInterface
+	 */
+	public function process(ServerRequestInterface $request, RequestHandlerInterface $handler):ResponseInterface
+	{
+		$response = parent::process($request, $handler);
+
+		// adding cache headers
+		$cacheUtils = new CacheUtil();
+		$response = $cacheUtils->withCache($response);
+
+		// adding eTag
+		if ($this->etag) {
+			$response = $cacheUtils->withETag($response, $this->etag);
+		}
+
+		// adding last modified
+		if ($this->lastModified) {
+			$response = $cacheUtils->withLastModified($response, $this->lastModified);
+			if ($cacheUtils->isNotModified($request, $response)) {
+				return new Response(304);
+			}
+		}
+
+		return $response;
+	}
+}
