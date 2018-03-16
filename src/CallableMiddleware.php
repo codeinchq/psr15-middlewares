f00e6281d243a5dfff038a04dae10141ca7f287a
Joan Fabrégat <joan@codeinc.fr>
2018-03-16 19:49:37 +0100

    adds CallableMiddleware

---
+++ src/CallableMiddleware.php
@@ -0,0 +1,74 @@
+<?php
+//
+// +---------------------------------------------------------------------+
+// | CODE INC. SOURCE CODE                                               |
+// +---------------------------------------------------------------------+
+// | Copyright (c) 2017 - Code Inc. SAS - All Rights Reserved.           |
+// | Visit https://www.codeinc.fr for more information about licensing.  |
+// +---------------------------------------------------------------------+
+// | NOTICE:  All information contained herein is, and remains the       |
+// | property of Code Inc. SAS. The intellectual and technical concepts  |
+// | contained herein are proprietary to Code Inc. SAS are protected by  |
+// | trade secret or copyright law. Dissemination of this information or |
+// | reproduction of this material  is strictly forbidden unless prior   |
+// | written permission is obtained from Code Inc. SAS.                  |
+// +---------------------------------------------------------------------+
+//
+// Author:   Joan Fabrégat <joan@codeinc.fr>
+// Date:     16/03/2018
+// Time:     19:47
+// Project:  Psr15Middlewares
+//
+declare(strict_types = 1);
+namespace CodeInc\Psr15Middlewares;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+
+/**
+ * Class CallableMiddleware
+ *
+ * @package CodeInc\Psr15Middlewares
+ * @author Joan Fabrégat <joan@codeinc.fr>
+ */
+class CallableMiddleware implements MiddlewareInterface
+{
+    /**
+     * @var callable
+     */
+    private $callable;
+
+    /**
+     * CallableMiddleware constructor.
+     *
+     * @param callable $callable
+     */
+    public function __construct(callable $callable)
+    {
+        $this->callable = $callable;
+    }
+
+    /**
+     * @inheritdoc
+     * @throws MiddlewareException
+     */
+    public function process(ServerRequestInterface $request,
+        RequestHandlerInterface $handler):ResponseInterface
+    {
+        try {
+            $response = call_user_func($this->callable, $request);
+        }
+        catch (\Throwable $exception) {
+            throw new MiddlewareException($this,
+                "Error while executing the callable");
+        }
+        if (!$response instanceof ResponseInterface) {
+            throw new MiddlewareException($this,
+                sprintf("The callable response must be an object implementing %s",
+                    ResponseInterface::class)):
+        }
+        return $response;
+    }
+}
