929d693eaf9d14b9d5fff8ae80d418d4091bc5de
Joan Fabrégat <joan@codeinc.fr>
2018-04-27 13:10:01 +0200

    new middleware NoCacheMiddleware

---
+++ src/HttpHeaders/NoCacheMiddleware.php
@@ -0,0 +1,52 @@
+<?php
+//
+// +---------------------------------------------------------------------+
+// | CODE INC. SOURCE CODE                                               |
+// +---------------------------------------------------------------------+
+// | Copyright (c) 2017 - Code Inc. SAS - All Rights Reserved.           |
+// | Visit https://www.codeinc.fr for more information about licensing.  |
+// +---------------------------------------------------------------------+
+// | NOTICE:  All information contained herein is, and remains the       |
+// | property of Code Inc. SAS. The intellectual and technical concepts  |
+// | contained herein are proprietary to Code Inc. SAS are protected by  |
+// | trade secret or copyright law. Dissemination of this information or |
+// | reproduction of this material  is strictly forbidden unless prior   |
+// | written permission is obtained from Code Inc. SAS.                  |
+// +---------------------------------------------------------------------+
+//
+// Author:   Joan Fabrégat <joan@codeinc.fr>
+// Date:     04/03/2018
+// Time:     08:38
+// Project:  Psr15Middlewares
+//
+declare(strict_types = 1);
+namespace CodeInc\Psr15Middlewares\HttpHeaders;
+use Micheh\Cache\CacheUtil;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+
+/**
+ * Class NoCacheMiddleware
+ *
+ * @uses CacheUtil
+ * @package CodeInc\Psr15Middlewares\HttpHeaders
+ * @author Joan Fabrégat <joan@codeinc.fr>
+ */
+class NoCacheMiddleware implements MiddlewareInterface
+{
+	/**
+	 * @inheritdoc
+	 * @param ServerRequestInterface $request
+	 * @param RequestHandlerInterface $handler
+	 * @return ResponseInterface
+	 */
+	public function process(ServerRequestInterface $request,
+        RequestHandlerInterface $handler):ResponseInterface
+	{
+		// returns the respknse with the cache prevention headers
+		return (new CacheUtil())->withCachePrevention($handler->handle($request));
+	}
+}
